21d467a857cfa520befe0c20e7ab9c2ac7806096
CR13524 <crei551@aucklanduni.ac.nz>
2025-08-06 13:20:28 +1200

    Added props

--- recipe.py
+++ recipe.py
@@ -1,19 +1,27 @@
 class Recipe:
-    def __init__(self):
-        self.__name = ""
-        self.__images = None
-        self.__ingredients = []
-        self.__author = ""
-        self.__date = ""
-        self.__rating = 0
-        self.__ingredient_quantities = []
-        self.__preparation_time = 0
-        self.__instructions = []
-        self.__category = None
-        self.__ID = ""
-        self.__cook_time = 0
-        self.__recipe_yield = 0
-        self.__servings = 0
-        self.__description = ""
-        self.__reviews = []
+    def __init__(self, name, images, ingredients, author,
+                 date, rating, ingredient_quantities, preperation_time,
+                 instructions, category, unique_id, cook_time, recipe_yield,
+                 servings, description, reviews=list):
+        self.__name = name
+        self.__images = images
+        self.__ingredients = ingredients
+        self.__author = author
+        self.__date = date
+        self.__rating = rating
+        self.__ingredient_quantities = ingredient_quantities
+        self.__preparation_time = preperation_time
+        self.__instructions = instructions
+        self.__category = category
+        self.__ID = unique_id
+        self.__cook_time = cook_time
+        self.__recipe_yield = recipe_yield
+        self.__servings = servings
+        self.__description = description
+        self.__reviews = reviews
+
+    def __repr__(self):
+        return (f"Recipe {self.__name} with id: {self.__ID} was created by {self.__author}"
+                f"on {self.__date}")
+
 
