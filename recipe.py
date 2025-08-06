7246718a3ab02f70b11cc9c6a70eab8ee41f1cbb
CR13524 <crei551@aucklanduni.ac.nz>
2025-08-06 12:43:40 +1200

    Created recipe class

---
+++ recipe.py
@@ -0,0 +1,12 @@
+class Recipe:
+    def __init__(self):
+        self.__unique_ID = ""
+        self.name = ""
+        self.description = ""
+        self.author = ""
+        self.ingredients = []
+        self.instructions = []
+        self.cooking_time = 0
+        self.category = None
+        self.other_details = {}
+
