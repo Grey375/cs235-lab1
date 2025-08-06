421bafc43a2d36d4cc51b6d381846387c3b2053a
CR13524 <crei551@aucklanduni.ac.nz>
2025-08-06 12:52:45 +1200

    Recipe Task1 complete

--- recipe.py
+++ recipe.py
@@ -1,12 +1,19 @@
 class Recipe:
     def __init__(self):
-        self.__unique_ID = ""
-        self.name = ""
-        self.description = ""
-        self.author = ""
-        self.ingredients = []
-        self.instructions = []
-        self.cooking_time = 0
-        self.category = None
-        self.other_details = {}
+        self.__name = ""
+        self.__images = None
+        self.__ingredients = []
+        self.__author = ""
+        self.__date = ""
+        self.__rating = 0
+        self.__ingredient_quantities = []
+        self.__preparation_time = 0
+        self.__instructions = []
+        self.__category = None
+        self.__ID = ""
+        self.__cook_time = 0
+        self.__recipe_yield = 0
+        self.__servings = 0
+        self.__description = ""
+        self.__reviews = []
 
