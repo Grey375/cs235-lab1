6fdbd94c298720ea8748dcb1da43b42316614daa
Grey375 <gmcc452@aucklanduni.ac.nz>
2025-08-06 12:52:57 +1200

    Category task1 complete

--- category.py
+++ category.py
@@ -1,5 +1,5 @@
 class Category:
     def __init__(self):
-        self.id = 0
-        self.name = ""
-        self.recipes = []
+        self.__id = 0
+        self.__name = ""
+        self.__recipes = []
